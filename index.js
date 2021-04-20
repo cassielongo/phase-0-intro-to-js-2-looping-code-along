9fd7ad9256016d603e87fa89f3cf63e0d2827d4f
Cassie Longo <longocassie@yahoo.com>
2021-04-19 19:31:29 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const newArray = [];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return newArray;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
+countDown(4);
